0b5e7bc97aa53219e486de481d62762fd1b80c5a
Spas Belev <spas@spass-macbook-pro.local>
2018-08-11 20:39:12 +0300

    Initial Commit

---
+++ ChatMe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatMe
+//
+//  Created by Spas Belev on 11.08.18.
+//  Copyright © 2018 Spas Belev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
